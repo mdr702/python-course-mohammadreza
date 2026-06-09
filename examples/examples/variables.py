23d896b08604c96a60cfacc334d926cbced8343d
mdr702 <mohamadi.mr68@gmail.com>
2026-06-09 18:17:17 +0330

    Create variables.py

---
+++ examples/examples/variables.py
@@ -0,0 +1,18 @@
+# متغیرها و انواع داده در پایتون
+
+name = "علی"        # str (رشته متنی)
+age = 25            # int (عدد صحیح)
+height = 1.78       # float (عدد اعشاری)
+is_student = True   # bool (منطقی)
+
+print("نام:", name)
+print("سن:", age)
+print("قد:", height)
+print("دانشجو است؟:", is_student)
+
+# نمایش نوع هر متغیر
+print("\nنوع داده‌ها:")
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
