1eba363d4946aa039a72417ba4d45f88a34c7acf
mdr702 <mohamadi.mr68@gmail.com>
2026-06-10 17:51:13 +0330

    Create exercise1.py

---
+++ part1-basics/exercises/exercise1.py
@@ -0,0 +1,19 @@
+"""
+Exercise 1 | تمرین ۱
+Simple Calculator | ماشین حساب ساده
+
+Write a program that:
+برنامه‌ای بنویسید که:
+1. Takes two numbers from user | دو عدد از کاربر بگیرد
+2. Prints sum, subtraction, multiplication, division | جمع، تفریق، ضرب، تقسیم را چاپ کند
+
+Example | مثال:
+Input: 10, 5
+Output:
+Sum | جمع: 15
+Subtraction | تفریق: 5
+Multiplication | ضرب: 50
+Division | تقسیم: 2.0
+"""
+
+# Your code here | کد خود را اینجا بنویسید
