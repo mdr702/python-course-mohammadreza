90f7234d81fa7350d7dffb6a925794541f1af349
mdr702 <mohamadi.mr68@gmail.com>
2026-06-10 17:55:08 +0330

    Create exercise6.py

---
+++ part1-basics/exercises/exercise6.py
@@ -0,0 +1,20 @@
+"""
+Exercise 6 | تمرین ۶
+Age Calculator | محاسبه سن دقیق
+
+Write a program that:
+برنامه‌ای بنویسید که:
+
+1. Takes birth year | سال تولد را بگیرد
+2. Takes current year | سال جاری را بگیرد
+3. Calculates exact age | سن دقیق را محاسبه کند
+4. Shows how many years until retirement (65) | چند سال تا بازنشستگی (۶۵ سال) نشان دهد
+
+Example | مثال:
+Birth year: 1990
+Current year: 2026
+Age: 36 years
+Years until retirement: 29 years
+
+Your code here | کد خود را اینجا بنویسید
+"""
