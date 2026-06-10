8c0f632d9838696cbd0241b694cd622463ab7c30
mdr702 <mohamadi.mr68@gmail.com>
2026-06-10 17:56:32 +0330

    Create exercise8.py

---
+++ part1-basics/exercises/exercise8.py
@@ -0,0 +1,20 @@
+"""
+Exercise 8 | تمرین ۸
+Digit Counter | شمارنده ارقام
+
+Write a program that:
+برنامه‌ای بنویسید که:
+
+1. Takes a number | یک عدد بگیرد
+2. Counts how many digits it has | تعداد ارقام آن را بشمارد
+3. Calculates sum of digits | جمع ارقام را محاسبه کند
+
+Example | مثال:
+Input: 12345
+Output: 5 digits, sum = 15
+
+Hint | راهنمایی: Convert to string and use len()
+نکته: به رشته تبدیل کن و از len() استفاده کن
+
+Your code here | کد خود را اینجا بنویسید
+"""
