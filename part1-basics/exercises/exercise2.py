c248c208c0e16213db241eceace76603f2659d2d
mdr702 <mohamadi.mr68@gmail.com>
2026-06-10 17:51:46 +0330

    Create exercise2.py

---
+++ part1-basics/exercises/exercise2.py
@@ -0,0 +1,17 @@
+"""
+Exercise 2 | تمرین ۲
+Age Group Detector | تشخیص گروه سنی
+
+Write a program that:
+برنامه‌ای بنویسید که:
+1. Takes age from user | سن را از کاربر بگیرد
+2. Shows age group | گروه سنی را نشان دهد
+
+Rules | قوانین:
+- Child | کودک: age < 13
+- Teenager | نوجوان: 13 <= age < 20
+- Adult | بزرگسال: 20 <= age < 60
+- Senior | سالمند: age >= 60
+"""
+
+# Your code here | کد خود را اینجا بنویسید
