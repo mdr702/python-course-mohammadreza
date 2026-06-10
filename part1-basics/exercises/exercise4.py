8bb52257db9617f7d21c826c4e8dee45afc8aa55
mdr702 <mohamadi.mr68@gmail.com>
2026-06-10 17:53:35 +0330

    Create exercise4.py

---
+++ part1-basics/exercises/exercise4.py
@@ -0,0 +1,20 @@
+"""
+Exercise 4 | تمرین ۴ (پیشرفته)
+Advanced Calculator | ماشین حساب پیشرفته
+
+Write a program that:
+برنامه‌ای بنویسید که:
+1. Takes two numbers | دو عدد بگیرد
+2. Takes operator (+, -, *, /) | عملگر (+, -, *, /) بگیرد
+3. Shows the result | نتیجه را نشان دهد
+
+Example | مثال:
+Enter first number | عدد اول: 10
+Enter operator | عملگر: *
+Enter second number | عدد دوم: 5
+Result | نتیجه: 50
+
+Hint | راهنمایی: Use if-elif or match-case
+"""
+
+# Your code here | کد خود را اینجا بنویسید
