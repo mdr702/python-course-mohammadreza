b88c8c4663897ccb4b969ba36f6d443ae05172bc
mdr702 <mohamadi.mr68@gmail.com>
2026-06-10 17:57:47 +0330

    Create exercise10.py

---
+++ part1-basics/exercises/exercise10.py
@@ -0,0 +1,33 @@
+"""
+Exercise 10 | تمرین ۱۰
+Star Patterns | الگوهای ستاره‌ای
+
+Write a program that prints these patterns:
+برنامه‌ای بنویسید که این الگوها را چاپ کند:
+
+Pattern 1 | الگوی ۱:
+*
+**
+***
+****
+*****
+
+Pattern 2 | الگوی ۲:
+    *
+   **
+  ***
+ ****
+*****
+
+Pattern 3 | الگوی ۳:
+*****
+****
+***
+**
+*
+
+Your code here | کد خود را اینجا بنویسید
+
+Hint | راهنمایی: Use nested loops and string multiplication
+از حلقه‌های تو در تو و ضرب رشته‌ها استفاده کن
+"""
