2bc2163cef1046fe465238da11b18032c6233514
mdr702 <mohamadi.mr68@gmail.com>
2026-06-10 17:56:03 +0330

    Create exercise7.py

---
+++ part1-basics/exercises/exercise7.py
@@ -0,0 +1,21 @@
+"""
+Exercise 7 | تمرین ۷
+Leap Year Checker | تشخیص سال کبیسه
+
+Write a program that:
+برنامه‌ای بنویسید که:
+
+1. Takes a year | یک سال بگیرد
+2. Checks if it's a leap year | بررسی کند کبیسه است یا نه
+
+Leap year rules | قوانین سال کبیسه:
+- Divisible by 4 | بر ۴ بخش‌پذیر باشد
+- If divisible by 100, must also be divisible by 400 | اگر بر ۱۰۰ بخش‌پذیر بود، باید بر ۴۰۰ هم بخش‌پذیر باشد
+
+Example | مثال:
+2024 → Leap year | کبیسه است
+1900 → Not leap year | کبیسه نیست
+2000 → Leap year | کبیسه است
+
+Your code here | کد خود را اینجا بنویسید
+"""
