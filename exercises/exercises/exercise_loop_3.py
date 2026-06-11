8e1584525bc08d902128d9d90357bfd151d13383
Mohammadreza (mdr702) <mohamadi.mr68@gmail.com>
2026-06-11 14:04:33 +0330

    Create exercise_loop_3.py

---
+++ exercises/exercises/exercise_loop_3.py
@@ -0,0 +1,9 @@
+"""
+Exercise 3: Multiplication Table
+تمرین ۳: جدول ضرب
+
+Ask the user for a number and print its multiplication table from 1 to 10.
+از کاربر یک عدد بخواهید و جدول ضرب آن را از ۱ تا ۱۰ چاپ کنید.
+"""
+
+# Your code here | کد خود را اینجا بنویسید
