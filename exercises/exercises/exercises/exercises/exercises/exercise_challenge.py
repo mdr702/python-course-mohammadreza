025e6c8f28fb0ca5bec24cf9fa36c006b6aed9cc
Mohammadreza (mdr702) <mohamadi.mr68@gmail.com>
2026-06-11 14:07:09 +0330

    Create exercise_challenge.py

---
+++ exercises/exercises/exercises/exercises/exercises/exercise_challenge.py
@@ -0,0 +1,11 @@
+"""
+Exercise 6: FizzBuzz Challenge
+تمرین ۶: چالش FizzBuzz
+
+Print numbers from 1 to 30. For multiples of 3, print "Fizz" instead of the number.
+For multiples of 5, print "Buzz". For numbers that are multiples of both 3 and 5, print "FizzBuzz".
+اعداد ۱ تا ۳۰ را چاپ کنید. برای مضرب‌های ۳، به جای عدد "Fizz" چاپ کنید.
+برای مضرب‌های ۵، "Buzz" و برای اعدادی که هم مضرب ۳ هستند و هم ۵، "FizzBuzz" چاپ کنید.
+"""
+
+# Your code here | کد خود را اینجا بنویسید
