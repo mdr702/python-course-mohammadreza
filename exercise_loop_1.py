b23e0c695da398647f8590f4be3f4e23cf46e547
Mohammadreza (mdr702) <mohamadi.mr68@gmail.com>
2026-06-11 14:02:29 +0330

    Create exercise_loop_1.py

---
+++ exercise_loop_1.py
@@ -0,0 +1,9 @@
+"""
+Exercise 1: Print Even Numbers
+تمرین ۱: چاپ اعداد زوج
+
+Write a program that prints all even numbers between 1 and 20.
+برنامه‌ای بنویسید که همه اعداد زوج بین ۱ تا ۲۰ را چاپ کند.
+"""
+
+# Your code here | کد خود را اینجا بنویسید
