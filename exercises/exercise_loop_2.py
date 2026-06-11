839e59bb8f03a3e6403787a4bc586b71bea55f29
Mohammadreza (mdr702) <mohamadi.mr68@gmail.com>
2026-06-11 14:03:45 +0330

    Create exercise_loop_2.py

---
+++ exercises/exercise_loop_2.py
@@ -0,0 +1,13 @@
+"""
+Exercise 2: Sum of a List
+تمرین ۲: جمع اعداد یک لیست
+
+Given the list numbers = [10, 20, 30, 40, 50], 
+calculate and print the sum of all its elements using a loop.
+لیست numbers = [10, 20, 30, 40, 50] را در نظر بگیرید.
+مجموع همه عناصر آن را با استفاده از یک حلقه حساب کرده و چاپ کنید.
+"""
+
+numbers = [10, 20, 30, 40, 50]
+
+# Your code here | کد خود را اینجا بنویسید
