2bb74d225680c010151195185a16b92946aa4848
Mohammadreza (mdr702) <mohamadi.mr68@gmail.com>
2026-06-11 14:05:34 +0330

    Create exercise_list_1.py

---
+++ exercises/exercises/exercises/exercise_list_1.py
@@ -0,0 +1,11 @@
+"""
+Exercise 4: Find the Largest Number
+تمرین ۴: پیدا کردن بزرگترین عدد
+
+Find the largest number in the list [3, 7, 2, 9, 1, 8] without using the max() function.
+بزرگترین عدد را در لیست [3, 7, 2, 9, 1, 8] بدون استفاده از تابع max() پیدا کنید.
+"""
+
+numbers = [3, 7, 2, 9, 1, 8]
+
+# Your code here | کد خود را اینجا بنویسید
