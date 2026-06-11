eb38e8b5c4b187f950c3f6e22476e3ed6f76e299
Mohammadreza (mdr702) <mohamadi.mr68@gmail.com>
2026-06-11 14:06:19 +0330

    Create exercise_list_2.py

---
+++ exercises/exercises/exercises/exercises/exercise_list_2.py
@@ -0,0 +1,11 @@
+"""
+Exercise 5: Remove Duplicates
+تمرین ۵: حذف مقادیر تکراری
+
+Write a program to remove duplicate values from the list [1, 2, 2, 3, 4, 4, 5].
+برنامه‌ای بنویسید که مقادیر تکراری را از لیست [1, 2, 2, 3, 4, 4, 5] حذف کند.
+"""
+
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+# Your code here | کد خود را اینجا بنویسید
