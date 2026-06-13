2d17e80676c075283b4e8f65131839073733de91
Mohammadreza (mdr702) <mohamadi.mr68@gmail.com>
2026-06-13 17:33:45 +0330

    Create exercise_dict_1.py

---
+++ exercises/exercises/exercise_dict_1.py
@@ -0,0 +1,17 @@
+"""
+Exercise 3: Lists to Dictionary | تمرین ۳: تبدیل لیست به دیکشنری
+
+Given two lists:
+keys = ["name", "age", "city"]
+values = ["Ali", 25, "Tehran"]
+
+Create a dictionary by combining these two lists.
+
+دو لیست داده شده است:
+یک دیکشنری با ترکیب این دو لیست بسازید.
+"""
+
+keys = ["name", "age", "city"]
+values = ["Ali", 25, "Tehran"]
+
+# Your code here | کد خود را اینجا بنویسید
