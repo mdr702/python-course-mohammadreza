154819666f7ba0c3c7298855d37992821dacbb17
Mohammadreza (mdr702) <mohamadi.mr68@gmail.com>
2026-06-13 17:33:13 +0330

    Create exercise_func_1.py

---
+++ exercises/exercise_func_1.py
@@ -0,0 +1,11 @@
+"""
+Exercise 1: Simple Function | تمرین ۱: تابع ساده
+
+Write a function called `add_numbers` that takes two parameters (a, b)
+and returns their sum.
+
+تابعی به نام `add_numbers` بنویسید که دو پارامتر (a, b) بگیرد
+و جمع آنها را برگرداند.
+"""
+
+# Your code here | کد خود را اینجا بنویسید
