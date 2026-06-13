790db50d2f152d599a7157e1661bb1921afa06cc
Mohammadreza (mdr702) <mohamadi.mr68@gmail.com>
2026-06-13 17:00:57 +0330

    Create functions_intro.py

---
+++ part3-functions-dicts/examples/functions_intro.py
@@ -0,0 +1,10 @@
+# Introduction to Functions | معرفی توابع
+
+def welcome():
+    print("Welcome to Python Functions!")
+    print("Functions help you write reusable code.")
+
+# Calling the function | فراخوانی تابع
+welcome()
+print("---")
+welcome()
