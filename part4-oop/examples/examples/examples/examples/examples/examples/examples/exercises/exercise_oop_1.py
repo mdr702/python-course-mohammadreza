d5775868439cf3f474843b5731202567d42d593f
Mohammadreza (mdr702) <mohamadi.mr68@gmail.com>
2026-06-15 17:36:32 +0330

    Create exercise_oop_1.py

---
+++ part4-oop/examples/examples/examples/examples/examples/examples/examples/exercises/exercise_oop_1.py
@@ -0,0 +1,33 @@
+"""
+Exercise 1: Student Class | تمرین ۱: کلاس دانشجو
+
+Create a Student class with the following:
+یک کلاس Student با ویژگی‌های زیر بسازید:
+
+Attributes (ویژگی‌ها):
+- name (نام)
+- age (سن)
+- student_id (شماره دانشجویی)
+- grades (list of grades | لیست نمرات)
+
+Methods (متدها):
+- add_grade(grade): adds a grade to the list
+- get_average(): returns the average of all grades
+- is_passing(): returns True if average >= 10
+- show_info(): prints all student information
+
+Extra (امتیازی):
+- Add validation: grade must be between 0 and 20
+- Track number of students created (class attribute)
+
+Example usage | مثال:
+student = Student("Ali", 20, "401001")
+student.add_grade(18)
+student.add_grade(15)
+student.add_grade(19)
+print(student.get_average())  # 17.33
+print(student.is_passing())   # True
+student.show_info()
+"""
+
+# Your code here | کد خود را اینجا بنویسید
